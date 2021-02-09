10b30a2e7f462b7afb1eb8ef4ccdc08617e083e2
Maureen Oldyck <maureen.oldyck@gmail.com>
2021-02-09 18:18:11 +0100

    Put header and footer in different files

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="footer fixed-bottom pt-1">
+    <p>Discovering Laravel: Made for BeCode Ghent</p>
+</footer>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,31 +8,12 @@
         <title>Laravel</title>
     </head>
     <body class="d-flex flex-column">
-        @yield('header')
-        <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
-            <a class="navbar-brand" href="/">Home</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-              <ul class="navbar-nav">
-                <li class="nav-item">
-                  <a class="nav-link"  href="/welcome">Welcome</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="/contact">Contact</a>
-                </li>
-              </ul>
-            </div>
-        </nav>
+        @include('header')
         <div class="container-fluid flex-fill flex-center position-ref full-height">
             <div class="content">
                 @yield('content')
             </div>
          </div>
-        @yield('footer')
-        <footer class="footer fixed-bottom pt-1">
-            <p>Discovering Laravel: Made for BeCode Ghent</p>
-        </footer>
+        @include('footer')
     </body>
 </html>
